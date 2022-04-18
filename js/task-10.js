448995d08db97b00c5da325d0853ea6ec83a5458
Anna Melnichuk <varchuk.anna.94@gmail.com>
2022-04-18 16:34:01 +0300

    Optional task done

--- js/task-10.js
+++ js/task-10.js
@@ -1,33 +1,44 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// };
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
-// const boxesContainerEl = document.getElementById('boxes');
-// const createBtn = document.querySelector('button[data-create]');
-// const destroyBtn = document.querySelector('button[data-destroy]');
-// const inputEl = document.getElementById('controls').firstElementChild;
+const boxesContainerEl = document.getElementById('boxes');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const inputEl = document.getElementById('controls').firstElementChild;
 
-// function createBoxes(amount) {
-//   const firstItem = document.createElement('div');
-//     firstItem.style.width = '20px';
-//     firstItem.style.height = '20px';
-//     firstItem.style.backgroundColor = 'pink';
-//     firstItem.style.marginBottom = '10px';
+function createBoxes(amount) {
+  const firstItem = document.createElement('div');
+    firstItem.style.width = '20px';
+    firstItem.style.height = '20px';
   
-//   let width = parseInt(firstItem.style.width);
-//   let height = parseInt(firstItem.style.height);
+  let width = 20;
+  let height = 20;
   
-//   for (let i = 1; i <= amount; i += 1) {
-//     width += 10;
-//     height += 10;
-//     const item = document.createElement('div');
-//     item.style.width = `${width}px`;
-//     item.style.height = `${height}px`;
-//     item.style.backgroundColor = 'pink';
-//     item.style.marginBottom = '10px';
-//     boxesContainerEl.append(item);
-//   }
-// };
+  for (let i = 1; i <= amount; i += 1) {
+    width += 10;
+    height += 10;
+    const item = document.createElement('div');
+    item.style.width = `${width}px`;
+    item.style.height = `${height}px`;
+    item.style.backgroundColor = getRandomHexColor();
+    item.style.marginTop = '10px';
+    boxesContainerEl.append(item);
+  }
+};
 
-// createBoxes(5);
+function destroyBoxes() {
+  boxesContainerEl.innerHTML = '';
+};
+
+
+destroyBtn.addEventListener('click', () => {
+  inputEl.value = '';
+  destroyBoxes();
+});
+
+createBtn.addEventListener('click', () => {
+  createBoxes(inputEl.value);
+});
+
 
